e781e3540249ad4dbe768d1a7e71b218f682b85a
Jimmy Jiang <jimmy.jiang@skyscanner.net>
2020-04-06 16:35:56 +0800

    使用线程池

---
+++ src/main/java/com/codve/pool/Server.java
@@ -0,0 +1,26 @@
+package com.codve.pool;
+
+import com.codve.block.Handler;
+
+import java.io.IOException;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.util.concurrent.ArrayBlockingQueue;
+import java.util.concurrent.ThreadPoolExecutor;
+import java.util.concurrent.TimeUnit;
+
+public class Server {
+    public static void main(String[] args) {
+        int port = 8080;
+        try (ServerSocket server = new ServerSocket(port)) {
+            ThreadPoolExecutor executor = new ThreadPoolExecutor(4, 8, 120L, TimeUnit.SECONDS,
+                    new ArrayBlockingQueue<>(64));;
+            while (true) {
+                Socket client = server.accept();
+                executor.execute(new Handler(client));
+            }
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+}
